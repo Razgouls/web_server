7ac6e0d2345fe739d164a4df86d632a179c63b40
eoliveir <eoliveir42@gmail.com>
2021-11-29 16:03:58 +0100

    finish : parser, cgi, chunked, multi-cgi

--- srcs/parser/main.cpp
+++ srcs/parser/main.cpp
@@ -6,7 +6,7 @@
 /*   By: elie <elie@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 14:22:40 by elie              #+#    #+#             */
-/*   Updated: 2021/11/29 13:37:03 by elie             ###   ########.fr       */
+/*   Updated: 2021/11/29 16:03:40 by elie             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 #include "ParserServer.hpp"
 #include "ParserRoute.hpp"
 #include "ConnexionServer.hpp"
-#include "FileConfig.hpp"
 
 ParserRoute						create_parser_route(std::ifstream &file_config, std::string &line)
 {
@@ -159,156 +158,12 @@ void	run(std::ifstream &file_config, std::string &path)
 	}
 }
 
-void	create_conf(void)
-{
-	std::list<std::string>	map_error;
-	map_error.push_back("401,./www/errors/401.html");
-	/*
-	** HEADER
-	*/
-	{FileConfig file("server{", 8005, "127.0.0.1", "default_server1", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server", 8005, "127.0.0.1", "default_server2", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server\t\t{", 8005, "127.0.0.1", "default_server3", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server    {", 8005, "127.0.0.1", "default_server4", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("", 8005, "127.0.0.1", "default_server5", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	
-	/*
-	** PORT
-	*/
-	{FileConfig file("server {", -2147483648, "127.0.0.1", "default_server6", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", -1, "127.0.0.1", "default_server7", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 0, "127.0.0.1", "default_server8", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	
-	/*
-	** HOST
-	*/
-	{FileConfig file("server {", 8005, "", "default_server9", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "\t\t127.0.01\t\t", "default_server10", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "   127.0.0.1   ", "default_server11", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.2.0.1", "default_server12", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "0.0.0.0", "default_server13", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "8.8.8.8", "default_server13", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-
-	/*
-	** SERVER NAME
-	*/
-	{FileConfig file("server {", 8005, "127.0.0.1", "", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-
-	/*
-	** ROOT
-	*/
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server14", "", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server15", "/elie", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server16", "./www/", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-
-	/*
-	** LIMIT CLIENT BODY SIZE
-	*/
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server17", "./www", -2147483648, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server18", "./www", -1, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-
-	/*
-	** ERROR
-	*/
-	map_error.clear();
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server19", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	
-	map_error.push_back(",./www/errors/401.html");
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server20", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	
-	map_error.clear();
-	map_error.push_back("./www/errors/401.html");
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server21", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	
-	map_error.clear();
-	map_error.push_back("401,");
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server22", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	
-	map_error.clear();
-	map_error.push_back("401");
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server23", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	
-	map_error.clear();
-	map_error.push_back("401,./www/elie/401.html");
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server24", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	map_error.clear();
-	map_error.push_back("401,./www/errors/401.html");
-	/*
-	** METHODS LOCATION
-	*/
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server25", "./www", 300000, map_error, "", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server26", "./www", 300000, map_error, "GET, POST, ELIE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server27", "./www", 300000, map_error, "GET, ", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-
-	/*
-	** PATH LOCATION
-	*/
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server28", "./www", 300000, map_error, "GET, POST, DELETE", "/", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server29", "./www", 300000, map_error, "GET, POST, DELETE", "/{", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server30", "./www", 300000, map_error, "GET, POST, DELETE", "/www {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server31", "./www", 300000, map_error, "GET, POST, DELETE", "/errors/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server32", "./www", 300000, map_error, "GET, POST, DELETE", "/errors/401.html {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server33", "./www", 300000, map_error, "GET, POST, DELETE", "/errors {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-
-	/*
-	** AUTOINDEX LOCATION
-	*/
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server34", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server35", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "elie", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server36", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "off", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-
-	/*
-	** INDEX LOCATION
-	*/
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server37", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server38", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "elie.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server39", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "cgi", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server40", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "/cgi", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-
-	/*
-	** UPLOAD DIR LOCATION
-	*/
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server41", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server42", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/elie", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server43", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads/", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .php");}
-
-	/*
-	** CGI BIN LOCATION
-	*/
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server44", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server45", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh,", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server46", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server47", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", ", /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server48", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", ",/usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server49", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/php-cgi,/usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server50", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/she, /usr/bin/php-cgi", ".sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server51", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgie", ".sh, .php");}
-
-	/*
-	** CGI EXTENSION LOCATION
-	*/
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server52", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", "");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server53", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh,      ");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server54", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh,");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server55", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ", .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server56", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".she, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server57", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, .phpe");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server58", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", "sh, .php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server59", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh, php");}
-	{FileConfig file("server {", 8005, "127.0.0.1", "default_server60", "./www", 300000, map_error, "GET, POST, DELETE", "/ {", "on", "index.html", "www/uploads", "/usr/bin/sh, /usr/bin/php-cgi", ".sh,.php");}
-}
-
 int		main(int argc, char **argv)
 {
 	std::string		file("./config/default.conf");
 	std::ifstream	ifs;
 
-	if (argc == 2 && strcmp(argv[1], "--gen-conf-file") == 0)
-	{
-		srand (time(NULL));
-		create_conf();
-		return (0);		
-	}
+	(void)argc;
 	if (argv[1])
 		file = argv[1];
 	ifs.open(file.c_str());
